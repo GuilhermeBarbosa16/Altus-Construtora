9db64af7fce820807b7d7d59f66c39419b8bcaa5
GuilhermeBarbosa16 <guiba1616@gmail.com>
2025-03-05 16:13:10 -0300

    fix: Novas imagens

--- src/components/PhotoSlider.tsx
+++ src/components/PhotoSlider.tsx
@@ -4,39 +4,61 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { Autoplay } from "swiper/modules";
-import foto from "../assets/foto.jpg"
-import foto1 from "../assets/foto1.jpg"
 
+// Import das imagens
+import IMG_01 from "../assets/IMG_01.jpg";
+import IMG_0409 from "../assets/IMG_0409.jpg";
+import IMG_1254 from "../assets/IMG_1254.jpg";
+import IMG_1256 from "../assets/IMG_1256.jpg";
+import IMG_1262 from "../assets/IMG_1262.jpg";
+import IMG_1286 from "../assets/IMG_1286.jpg";
+import IMG_1382 from "../assets/IMG_1382.jpg";
+import IMG_1569 from "../assets/IMG_1569.jpg";
+import IMG_2088 from "../assets/IMG_2088.jpg";
+import IMG_2202 from "../assets/IMG_2202.jpg";
+import IMG_2964 from "../assets/IMG_2964.jpg";
+import IMG_2965 from "../assets/IMG_2965.jpg";
+import IMG_3361 from "../assets/IMG_3361.jpg";
+import IMG_8867 from "../assets/IMG_8867.jpg";
+
+// Array de imagens
 const images = [
-  foto,
-  foto1,
-  foto,
-  foto1,
-  foto,
-  foto1
+  { name: "IMG_01", src: IMG_01 },
+  { name: "IMG_0409", src: IMG_0409 },
+  { name: "IMG_1254", src: IMG_1254 },
+  { name: "IMG_1256", src: IMG_1256 },
+  { name: "IMG_1262", src: IMG_1262 },
+  { name: "IMG_1286", src: IMG_1286 },
+  { name: "IMG_1382", src: IMG_1382 },
+  { name: "IMG_1569", src: IMG_1569 },
+  { name: "IMG_2088", src: IMG_2088 },
+  { name: "IMG_2202", src: IMG_2202 },
+  { name: "IMG_2964", src: IMG_2964 },
+  { name: "IMG_2965", src: IMG_2965 },
+  { name: "IMG_3361", src: IMG_3361 },
+  { name: "IMG_8867", src: IMG_8867 },
 ];
-
 
 export default function PhotoSlider() {
   return (
     <div className="w-full flex justify-center">
       <Swiper
-        slidesPerView={4} // Exibe 3 imagens ao mesmo tempo
-        spaceBetween={5} // Espaçamento suave entre imagens
+        slidesPerView={4} // Ajuste para ver se carrega mais de uma imagem
+        spaceBetween={10}
         autoplay={{ delay: 2000, disableOnInteraction: false }}
-        loop={true} // Faz o loop infinito das imagens
+        loop={true}
         modules={[Autoplay]}
         className="w-auto"
       >
-        {images.map((src, index) => (
-          <SwiperSlide key={index} className="flex justify-center">
-            <img
-              src={src}
-              alt={`Slide ${index}`}
-              className="w-[200px] h-auto rounded-lg shadow-md"
-            />
-          </SwiperSlide>
-        ))}
+        {images.map((image, index) => (
+  <SwiperSlide key={index} className="flex justify-center">
+    <img              
+      src={image.src} // Aqui estamos garantindo que a imagem está sendo usada corretamente
+      alt={image.name}
+      className="w-[450px] h-auto rounded-lg shadow-md"
+    />
+  </SwiperSlide>
+))}
       </Swiper>
     </div>
   );
